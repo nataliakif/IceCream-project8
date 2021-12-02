7995d03746b0d523371eb9ce7e00bd11adf54ca7
szhosan <34851087+szhosan@users.noreply.github.com>
2021-12-02 08:15:03 +0000

    Deploying to gh-pages from @ nataliakif/IceCream-project8@7a4eaf346f1c374a33dcf981b5df333878ff785c 🚀

--- src.0b927e34.js
+++ src.0b927e34.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/IceCream-project8/src.4e2e246d.js.map
+//# sourceMappingURL=/IceCream-project8/src.0b927e34.js.map
