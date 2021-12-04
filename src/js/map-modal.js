ed22f5a66b89e291f0dc30218cf736d51ec96a13
@s_zhsn <s.zhosan@gmail.com>
2021-12-05 00:54:31 +0200

    google-maps-low-performance-fix

--- src/js/map-modal.js
+++ src/js/map-modal.js
@@ -9,8 +9,11 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  function aload(t){"use strict";var e="data-aload";return t=t||window.document.querySelectorAll("["+e+"]"),void 0===t.length&&(t=[t]),[].forEach.call(t,function(t){t["LINK"!==t.tagName?"src":"href"]=t.getAttribute(e),t.removeAttribute(e)}),t}
+
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
     refs.bodyScrollLock.classList.toggle('modal-open');
+    aload();
   }
 })();
